91478dc49fdb5d5f31eeee07f9b4b2747d96c6f2
juanchensh <juanchensh@quarkfinance.com>
2018-01-30 16:34:52 +0800

    idea整合git6测试

--- src/com/july/demo/HelloWorld.java
+++ src/com/july/demo/HelloWorld.java
@@ -2,7 +2,7 @@ package com.july.demo;
 
 public class HelloWorld {
     public static void main(String[] args){
-        System.out.println("july s thirds5555 idea project");
+        System.out.println("july s thirds6666idea project");
     }
 
 }
