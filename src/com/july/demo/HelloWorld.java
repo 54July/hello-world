29fa9252b33a0b208fa9506908d652ef25a52755
54July <ajmd123>
2018-03-24 23:57:11 +0800

    idea testing

--- src/com/july/demo/HelloWorld.java
+++ src/com/july/demo/HelloWorld.java
@@ -2,7 +2,9 @@ package com.july.demo;
 
 public class HelloWorld {
     public static void main(String[] args){
+
         System.out.println("july s thirds6666idea project");
+        System.out.println("wfyb");
     }
 
 }
